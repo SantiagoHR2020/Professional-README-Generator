0c0e54810e7d338e15928abc74870e77492dec79
Santiago Hincapie <santiagohr81@hotmail.com>
2020-11-07 01:24:46 -0500

    basic structure

---
+++ index.js
@@ -0,0 +1,20 @@
+const inquirer = require("inquirer")
+
+// array of questions for user
+
+
+const questions = [
+
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+    inquirer.prompt(questions)
+}
+
+// function call to initialize program
+init();
